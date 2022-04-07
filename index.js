bab282dd959a631f5ff93d3ef1c51ab15cd47dd1
Eli <elipuga2@gmail.edu>
2022-04-06 22:34:22 -0500

    week 2 part 1

---
+++ index.js
@@ -0,0 +1,15 @@
+const PORT = 3000;
+const express = require('express');
+const morgan = require('morgan');
+const { client } = require('./db');
+const server = express();
+client.connect();
+server.use(morgan('dev'));
+server.use(express.json())
+
+const apiRouter = require('./api');
+server.use('/api', apiRouter)
+
+server.listen(PORT, () => {
+  console.log('The server is up on port', PORT)
+});
